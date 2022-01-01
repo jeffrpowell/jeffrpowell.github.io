3d708a24ca8d56041e4e39f9c50fe5265b377e18
jeffrpowell <jeffr.powell@outlook.com>
2022-01-01 13:59:03 -0700

    automated update to ng 11

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
 const routes: Routes = [
-  { path: 'whois', loadChildren: () => import('./about-me/about-me.module').then(m => m.AboutMeModule) }, 
-  { path: 'tech', loadChildren: () => import('./tech/tech.module').then(m => m.TechModule) }, 
-  { path: 'projects', loadChildren: () => import('./projects/projects.module').then(m => m.ProjectsModule) }, 
+  { path: 'whois', loadChildren: () => import('./about-me/about-me.module').then(m => m.AboutMeModule) },
+  { path: 'tech', loadChildren: () => import('./tech/tech.module').then(m => m.TechModule) },
+  { path: 'projects', loadChildren: () => import('./projects/projects.module').then(m => m.ProjectsModule) },
   { path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) },
-  { path: '**', redirectTo: 'whois', pathMatch: "full" }, 
+  { path: '**', redirectTo: 'whois', pathMatch: 'full' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
