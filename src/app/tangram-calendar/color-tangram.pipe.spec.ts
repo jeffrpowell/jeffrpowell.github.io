f308675383a034c3fe8bf510506557ad2014584e
Jeff Powell <github@jeffpowell.dev>
2025-07-26 13:41:18 -0600

    deleting all the angular code

--- src/app/tangram-calendar/color-tangram.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ColorTangramPipe } from './color-tangram.pipe';
-
-describe('ColorTangramPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ColorTangramPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
